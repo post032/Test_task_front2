37b02ac67fd2f67e426d98de7abc9b06e185998c
Elena Ordina <post032@yandex.ru>
2018-12-18 16:54:46 +0300

    доработка кнопок

--- source/js/goods.js
+++ source/js/goods.js
@@ -26,17 +26,30 @@ function loadProducts() {
       out += '<div class="products__status">Наличие' + '</div>';
       out += '<div class="products__coast">';
       out += '<div class="products__one">';
-      out += '<div class="products__m" autofocus>' + '<span class="products__cards">По карте клуба' + '</span>' + data[key]["priceGold"].toFixed(2) + '</div>';
-      out += '<div class="products__m">' + data[key]["priceRetail"].toFixed(2) + '</div>';
+      out += '<div class="products__m product__m">' + '<span class="products__cards">По карте клуба' + '</span>' + data[key]["priceGold"].toFixed(2) +
+      '<svg version="1.0" id="rouble__b" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="30px" height="22px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">'+
+        '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#rouble_black">'+'</use>'+
+      '</svg>' + '</div>';
+      out += '<div class="products__m">' + data[key]["priceRetail"].toFixed(2) +
+      '<svg version="1.0" id="rouble__g" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="30px" height="22px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">' +
+        '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#rouble_gray">'+'</use>'+
+      '</svg>'+
+      '</div>';
       out += '</div>'
       out += '<div class="products__two">';
-      out += '<div class="products__p">' + '<span class="products__cards">По карте клуба' + '</span>' + data[key]["priceGoldAlt"].toFixed(2) + '</div>';
-      out += '<div class="products__p">' + data[key]["priceRetailAlt"].toFixed(2) + '</div>';
+      out += '<div class="products__p product__p">' + '<span class="products__cards">По карте клуба' + '</span>' + data[key]["priceGoldAlt"].toFixed(2) +
+      '<svg version="1.0" id="rouble__b" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="30px" height="22px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">'+
+        '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#rouble_black">'+'</use>'+
+      '</svg>' + '</div>';
+      out += '<div class="products__p">' + data[key]["priceRetailAlt"].toFixed(2) +
+      '<svg version="1.0" id="rouble__g" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="30px" height="22px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">' +
+        '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#rouble_gray">'+'</use>'+
+      '</svg>'+ '</div>';
       out += '</div>';
       out += '</div>';
       out += '<div class="products__bonus">Можно купить за ' + data[key]["bonusAmount"] + ' балла</div>';
       out += '<div class="products__btn">';
-      out += '<button class="products__metre" id="metre">За М. кв.' + '</button>';
+      out += '<button class="products__metre">За М. кв.' + '</button>';
       out += '<button class="products__pack">За упаковку' + '</button>';
       out += '</div>';
       out += '<div class="products__cart">';
@@ -56,13 +69,17 @@ function loadProducts() {
     $("#products_section2").html(out);
     $('button.products__basket').on('click', addToCart);
 
-    //показываем блоки переключения
+    // показываем блоки переключения
     $('.products__metre').on('click', function() {
+      $('.products__pack').removeClass('selection');
+      $('.products__metre').toggleClass('selection');
       $('.products__one').show();
       $('.products__two').hide();
     });
 
     $('.products__pack').on('click', function() {
+      $('.products__metre').removeClass('selection');
+      $('.products__pack').toggleClass('selection');
       $('.products__two').show();
       $('.products__one').hide();
     });
@@ -76,6 +93,9 @@ function loadProducts() {
     $('.products__down').click(function() {
       $('.products__count').val($('.products__count').val() - 1);
     });
+
+
+
 
   });
 
